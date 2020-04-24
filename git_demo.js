0d22b4a714a63000ef427bdcc3192a9ee7c07131
SHITTU YASIR IDRIS <talk2shittuyasir@gmail.com>
2020-04-24 14:09:48 +0100

    append function created

--- git_demo.js
+++ git_demo.js
@@ -3,3 +3,13 @@ const fs = require('fs');
 const myWriter = fs.writeFile('note.txt', 'The first content', () => {
     console.log(`file created`);
 });
+
+
+let myAppender = fs.appendFile('note.txt', 'content from the appendbranch', (err) => {
+    if (err) {
+        throw err;
+        return;
+    }
+
+    console.log('data appended');
+});
